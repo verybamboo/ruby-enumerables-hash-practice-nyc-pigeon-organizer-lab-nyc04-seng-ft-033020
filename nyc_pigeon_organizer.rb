473cb70a823d5e2303e2b5deda161c20ddc6b7c1
George Chen <georgechen1338@gmail.com>
2020-03-24 04:57:47 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,23 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  pigeon_hash = {}
+  
+  data.each do |key, value|
+    value.each do |new_value, pigeons|
+      pigeons.each do |pigeons|
+        
+        if !pigeon_hash[pigeons]
+          pigeon_hash[pigeons] = {}
+        end
+
+        if !pigeon_hash[pigeons][key]
+          pigeon_hash[pigeons][key] = []
+        end
+
+        pigeon_hash[pigeons][key] << new_value.to_s
+
+      end
+    end
+  end
+  pigeon_hash
 end
